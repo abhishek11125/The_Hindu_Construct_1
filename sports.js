57e39120bdd82ef5b6a60160893cee17e9586509
abhishek11125 <103635309+abhishek11125@users.noreply.github.com>
2022-06-18 23:05:14 +0530

    added footer section

--- sports.js
+++ sports.js
@@ -157,3 +157,82 @@ article.forEach(function(elem){
     document.querySelector("#otherarticle").append(cont1);
 
 })
+
+
+
+
+let arr=[{url:"https://www.thehindu.com/incoming/pzjvi/article65338242.ece/alternates/LANDSCAPE_320/WikiLeaks_32414.jpg-063d2.jpg",
+para:"U.K. goverment approves extradition of Julian Assange to U.S, Wikileaks says he will appeal"},
+{url:"https://www.thehindu.com/incoming/4eeus1/article65512036.ece/alternates/LANDSCAPE_100/AFP_32C78Q2.jpg",
+para:"U.S says it 'condemns remarks on prophet made by BJP officials'"},
+{url:"https://www.thehindu.com/incoming/mob4i/article65532467.ece/alternates/LANDSCAPE_100/APTOPIX_Ukraine_Russia_War_99649.jpg-935c4.jpg",
+para:"Ukraine crisis: Leaders of France, Germany and Italy head for Kyivas U.S. pledges more military aid"},
+{url:"https://www.thehindu.com/incoming/xtue23/article65536509.ece/alternates/LANDSCAPE_320/2022-06-17T045048Z_1364658127_RC2FTU9ITNH0_RTRMADP_3_TRADE-WTO.JPG",
+para:"Intellectual property rights waiver falls short of expectation at WTO meeting"},
+{url:"https://www.thehindu.com/news/national/hgxi0q/article65536488.ece/alternates/LANDSCAPE_100/IMG_TH16_KRA_SHEKAWAT_2_1_6K864S6N.jpg",
+para:"BJP appoints 14-member panel to oversee Presidential polls, 'guide' MPs on voting"},
+{url:"https://www.thehindu.com/incoming/x38emt/article65529950.ece/alternates/LANDSCAPE_100/PTI06_15_2022_000163A.jpg",
+para:" Presidential polls | People may ask how Opposition will give capable PM if it can't field strong candidate ..."},
+]
+
+    //   Moving - 1
+
+let image=document.querySelector("#moving>img");
+let content=document.querySelector("#moving>p");
+let button=document.querySelectorAll("#moving>button");
+
+button[0].addEventListener("click",display_left)
+let num=0;
+
+function display_left(){
+    if(num==0){
+        num=arr.length-1;
+    }
+    else{
+        num--;
+    }
+    image.src=arr[num].url;
+    content.innerText=arr[num].para;
+}
+
+button[1].addEventListener("click",function(){
+    if(num==arr.length-1){
+        num=0;
+    }
+    else{
+        num++;
+    }
+    image.src=arr[num].url;
+    content.innerText=arr[num].para;
+});
+
+    //   Moving - 2
+
+let image2=document.querySelector("#moving2>img");
+let content2=document.querySelector("#moving2>p");
+let button2=document.querySelectorAll("#moving2>button");
+
+button2[0].addEventListener("click",display_left2)
+let num2=arr.length-1;
+
+function display_left2(){
+    if(num2==0){
+        num2=arr.length-1;
+    }
+    else{
+        num2--;
+    }
+    image2.src=arr[num2].url;
+    content2.innerText=arr[num2].para;
+}
+
+button2[1].addEventListener("click",function(){
+    if(num2==arr.length-1){
+        num2=0;
+    }
+    else{
+        num2++;
+    }
+    image2.src=arr[num2].url;
+    content2.innerText=arr[num2].para;
+});
